2762189d92886effb14d591d22a7bd5fb4b4b891
Rachel Denton <rdenton3@terpmail.umd.edu>
2021-12-29 10:50:17 -0500

    display dynamic city info on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,58 @@
+var formEl = document.querySelector("#user-form")
+var cityInputEl = document.querySelector("#city")
+var displayContainerEl = document.querySelector("#display-container")
+var searchEl = document.querySelector("#city-search")
+
+"https://api.openweathermap.org/data/2.5/weather?q=new_york&units=imperial&appid=d40c76615a039246eaf9d8955c56e72e";
+var formSubmit = function(event) {
+    event.preventDefault();
+    // get value from the form input
+    var city = cityInputEl.value.trim()
+    // if city is entered, then call weather API function with city coordinates
+    if (city) {
+        cityPull(city)
+        // then clear form input
+        displayContainerEl.textContent = ""
+        cityInputEl.value = "";
+    }
+    // else alert user they have not entered a value
+    else {
+        alert("Please enter a city name.")
+    }
+    // console.log(city)
+}
+
+// take user's city input and feed through API to get lon and lat
+var cityPull = function(city){
+    apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=d40c76615a039246eaf9d8955c56e72e"
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            displayCity(city)
+            // retrieve the lat and lon coordinates to run through weatherPull and get weather data
+            var lat = data.coord.lat
+            var lon = data.coord.lon
+            weatherPull(lat, lon)
+        })
+    })
+
+}
+// call weather function dynamically based on user inputs
+var weatherPull = function(lat, lon) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,daily&appid=50ee644cfafc3cd04f5298fe9bd700dd"
+}
+
+var displayCity = function(city) {
+    // first clear existing content
+    displayContainerEl.textContent = ""
+    searchEl.textContent = city
+}
+
+var displayWeather = function(data) {
+
+}
+
+formEl.addEventListener("submit", formSubmit)
+
 // // fetch the weather api and retrieve json data as array
 // var getWeather = function() {
 //     var response = fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=50ee644cfafc3cd04f5298fe9bd700dd")
@@ -9,36 +64,5 @@
 // };
   
 // getWeather();
-var formEl = document.querySelector("#user-form")
-var cityInputEl = document.querySelector("#city")
 
-var formSubmit = function(event) {
-    // prevent default behavior which for form element would be to send form input data to a URL
-    // we want to handle the form input data in js
-    event.preventDefault();
-    console.log("log event")
-    // get value from the form input
-    var city = cityInputEl.value.trim().toLowerCase()
-    // if city is entered, then call weather API function with city coordinates
-    if (city) {
-        weatherPull(33.44,-94.04)
-    }
-    // else alert user they have not entered a value
-    else {
-        alert("Please enter a city name")
-    }
-    console.log(city)
-}
-// call weather function dynamically based on user inputs
-var weatherPull = function(lat, lon) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,daily&appid=50ee644cfafc3cd04f5298fe9bd700dd"
 
-    fetch(apiUrl).then(function(response){
-        response.json().then(function(data){
-            console.log(data)
-        })
-    })
-}
-
-// weatherPull(33.44,-94.04)
-formEl.addEventListener("submit", formSubmit)
